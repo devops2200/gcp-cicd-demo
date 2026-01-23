6a276bd30d11a73d08c7e1eadefd7c9d149d9083
devops2200 <118130760+devops2200@users.noreply.github.com>
2026-01-23 14:30:49 +0200

    Revert files

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
-
 @app.route("/")
-def hello():
-    # We changed the response to HTML to verify deployment
-    return "<h1>Kaniko Build & Deploy Successful!</h1>"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+def hello(): return "Hello from Cloud Build!"
+if __name__ == "__main__": app.run(host="0.0.0.0", port=8080)
